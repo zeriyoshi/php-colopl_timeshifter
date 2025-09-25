e0b0d1f6a65d90622d5ee2c38f7acd06feb1d7df
Go Kudo <g-kudo@colopl.co.jp>
2025-09-25 11:22:00 +0900

    f

--- library_test.sh
+++ library_test.sh
@@ -1,6 +1,14 @@
 #!/bin/sh -eu
 
 cd "/project"
+  cd "ext"
+    phpize
+    ./configure
+    make -j"$(nproc)"
+    TEST_PHP_ARGS="--show-diff -q" make test
+    make install
+    docker-php-ext-enable "colopl_timeshifter"
+  cd -
   composer install
   composer exec -- phpunit "tests"
   composer exec -- phpstan
